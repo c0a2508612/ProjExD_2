e4331f2d7db95534052c328ee59305a3e27768b7
c0a2508612 <c0a2508612@apps.teu.ac.jp>
2026-04-21 16:24:23 +0900

    演習1: ゲームオーバー画面を追加

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,10 +2,12 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """入力されたRectの座標をチェックして、画面外であればTrue、画面内ではFalseを返す。
@@ -21,10 +23,11 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
         yoko = True
     if rct.top < 0 or rct.bottom > HEIGHT:
         tate = True
-    
+
     return yoko, tate
 
-def move_bound(rct: pg.Rect):
+
+def move_bound(rct: pg.Rect) -> None:
     """入力されたRectの座標をチェックして、画面外であれば移動をブロックする。(独自実装)
 
     Args:
@@ -39,10 +42,56 @@ def move_bound(rct: pg.Rect):
     if rct.right > WIDTH:
         rct.right = WIDTH
 
+    return None
+
+
+def show_gameover(screen: pg.Surface) -> None:
+    # ゲームオーバーのスクリーンを定義
+    gameover_screen = pg.Surface((WIDTH, HEIGHT))
+    # 背景を設定
+    gameover_bg_img = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(gameover_bg_img, (0, 0, 0), pg.Rect(0, 0, WIDTH, HEIGHT))
+    # スクリーンの透明度を設定
+    gameover_screen.set_alpha(200)
+    # フォントを指定
+    gameover_font = pg.font.Font(None, 128)
+    # テキスト内容と色を指定
+    gameover_text = gameover_font.render("GAME OVER", True, (255, 255, 255))
+    # 泣く画画像読み込み
+    kk_cry_img = pg.image.load("fig/8.png")
+    # 文字を画面中央に配置
+    gameover_screen.blit(
+        gameover_text,
+        (
+            int(WIDTH / 2 + -gameover_text.get_width() / 2),
+            int(HEIGHT / 2 + -gameover_text.get_height() / 2),
+        ),
+    )
+    # 画像を文字の左右に配置
+    gameover_screen.blit(
+        kk_cry_img,
+        (
+            int(WIDTH / 2 + -kk_cry_img.get_width() / 2) + 360,
+            int(HEIGHT / 2 + -kk_cry_img.get_height() / 2),
+        ),
+    )
+    gameover_screen.blit(
+        kk_cry_img,
+        (
+            int(WIDTH / 2 + -kk_cry_img.get_width() / 2) + -360,
+            int(HEIGHT / 2 + -kk_cry_img.get_height() / 2),
+        ),
+    )
+
+    screen.blit(gameover_screen, (0, 0))
+
+    return None
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
@@ -53,8 +102,8 @@ def main():
     # bb_imgの背景色を抜く(カラーキー)
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
-    # 初期座標はランダム
-    bb_rct.move_ip(random.randint(0, WIDTH), random.randint(0, HEIGHT))
+    # 初期座標はランダム (きわどい座標だと爆弾が動かないので若干範囲を絞る)
+    bb_rct.move_ip(random.randint(2, WIDTH - 2), random.randint(2, HEIGHT - 2))
     # 爆弾の移動速度を定義
     vx, vy = +5, +5
     clock = pg.time.Clock()
@@ -63,13 +112,13 @@ def main():
         pg.K_UP: (0, -5),
         pg.K_DOWN: (0, 5),
         pg.K_LEFT: (-5, 0),
-        pg.K_RIGHT: (5, 0)
+        pg.K_RIGHT: (5, 0),
     }
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -81,8 +130,8 @@ def main():
         kk_rct.move_ip(sum_mv)
         # この方法だとX,Y軸どちらかが衝突した場合に、衝突していない片方の軸の移動ができなくなる
         if check_bound(kk_rct) != (False, False):
-             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
-        
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         # move_bound(kk_rct)
         screen.blit(kk_img, kk_rct)
         # 爆弾をうごかす
@@ -93,8 +142,12 @@ def main():
         if tate:
             vy *= -1
         screen.blit(bb_img, bb_rct)
+        # 衝突時の処理
         if kk_rct.colliderect(bb_rct):
             print("DEAD")
+            show_gameover(screen)
+            pg.display.update()
+            time.sleep(5)
             return
         pg.display.update()
         tmr += 1
